f2c344c137336c8caeba1560055fdb05a785c3bb
Amirhamja422 <amirhamja422@gmail.com>
2023-02-19 22:46:04 +0600

    new

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
 <body>
-
-<h2>My First JavaScript</h2>
 
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
 
-<p id="demo"></p>
+<!-- <p id="demo"></p> -->
 
 </body>
-</html> 
+</html>
