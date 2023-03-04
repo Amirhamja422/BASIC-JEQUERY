22c93c9fbba3d0fb8adcb61ff26eda71656d1f84
Amirhamja422 <amirhamja422@gmail.com>
2023-03-04 23:17:21 +0600

    array loop

--- index.php
+++ index.php
@@ -1,18 +1,71 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
+  <?php
 
-<div>
-  <p class="name">freeCodeCamp</p>
-  <button onclick="changeColor()">Change to Blue</button>
-</div>
+  ?>
 
-<!-- <p id="demo"></p> -->
+
+  <script>
+    /*
+    using for loop 
+ 
+ */
+    let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // $s=0;
+    // for(let i = 0; i < num.length; i++) 
+    //   {
+    //        console.log($s++);
+    //       console.log((num[i]));
+    //   }
+
+
+    /*
+     using foreach loop 
+
+     */
+
+    //  num.forEach((element)=>{
+    //    console.log(element*element);
+    //    console.log(element);
+
+    //  })
+
+    /*
+    The Array.from() method returns an array from any iterable object
+    Create an array from a string
+    string convert into array 
+
+     */
+
+
+    //  let text = "amir";
+    //  const myArr = Array.from(text);
+    //  console.log(myArr);
+
+    //  output: 
+    //  (4) ['a', 'm', 'i', 'r']
+
+    /*
+      loop shortcart 
+    */
+
+    // for(let i of num){
+    //   console.log ( i );
+    // }
+   
+
+  </script>
+
+  <!-- <p id="demo"></p> -->
 
 </body>
+
 </html>
